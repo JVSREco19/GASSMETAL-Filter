01ed82be451b0ff042b0c00364a115ea0bd8dd5d
JVSREco19 <joao.victor.silveira.r@unifei.edu.br>
2022-10-09 11:29:34 -0300

    Test all .txt files that dont start with 'treated'

--- teste.py
+++ teste.py
@@ -1,4 +1,5 @@
 import re
+import os
 
 def remove_repetidos(lista):
     l = []
@@ -13,51 +14,54 @@ def remove_repetidos(lista):
     print(str(removeCounter)+ " Removed")
     return l
 
+for filename in os.listdir(os.getcwd()):
+  
+  archiveName =  filename
+  if archiveName[len(archiveName)-3:len(archiveName)] =='txt' and archiveName[0:7]!='treated':
+    print("Opening {} file".format(archiveName))
+    ficheiro = open(archiveName)
+    text = ficheiro.readlines()
+    linesToMantain = []
 
-archiveName = 'ActiveSitesFound.txt'
-ficheiro = open(archiveName)
-text = ficheiro.readlines()
-linesToMantain = []
-
-if(archiveName[-3:]=='csv'):
-  text = text[2:]
-
-
-activeSites = []
-counter = 0;
-textList =[]
-for linha in text:
-  words = linha.split()
-  textList.append(linha.split("\t"))
-  stringWithSpaces= " ".join(words[3:])
-  templateMatch = re.search(r'[a-zA-Z][a-zA-Z0-9]{3}|[a-zA-Z0-9][a-zA-Z][a-zA-Z0-9]{2}|[a-zA-Z0-9]{2}[a-zA-Z][a-zA-Z0-9]|[a-zA-Z0-9]{3}[a-zA-Z]',stringWithSpaces)
-  templatePos = templateMatch.start()
-  templateCode = templateMatch.group()
-  ligationsString = stringWithSpaces[:templatePos-1]
-  finalArray= ligationsString.split(';')
-  finalArray.append(templateCode)
-  activeSites.insert(counter,finalArray)
-  activeSites[counter].sort()
-  activeSites[counter].append(str(counter))
-  counter= counter +1;
-counter = 0;
-
-activeSites= remove_repetidos(activeSites)
-activeSites.sort()
-
-treatedText = []
-for num in linesToMantain:
-  if(counter==100):
-    counter =0
-  treatedLine = textList[num]
-  treatedLine[1] = str(counter);
-  counter += 1;
-  treatedText.append(' '.join(treatedLine))
-ficheiro.close()
-treatedFile = open("treated"+archiveName,"w")
-
-for member in treatedText:
-  treatedFile.write(member)
+    if(archiveName[-3:]=='csv'):
+      text = text[2:]
 
 
-treatedFile.close()
+    activeSites = []
+    counter = 0;
+    textList =[]
+    for linha in text:
+      words = linha.split()
+      textList.append(linha.split("\t"))
+      stringWithSpaces= " ".join(words[3:])
+      templateMatch = re.search(r'[a-zA-Z][a-zA-Z0-9]{3}|[a-zA-Z0-9][a-zA-Z][a-zA-Z0-9]{2}|[a-zA-Z0-9]{2}[a-zA-Z][a-zA-Z0-9]|[a-zA-Z0-9]{3}[a-zA-Z]',stringWithSpaces)
+      templatePos = templateMatch.start()
+      templateCode = templateMatch.group()
+      ligationsString = stringWithSpaces[:templatePos-1]
+      finalArray= ligationsString.split(';')
+      finalArray.append(templateCode)
+      activeSites.insert(counter,finalArray)
+      activeSites[counter].sort()
+      activeSites[counter].append(str(counter))
+      counter= counter +1;
+    counter = 0;
+
+    activeSites= remove_repetidos(activeSites)
+    activeSites.sort()
+
+    treatedText = []
+    for num in linesToMantain:
+      if(counter==100):
+        counter =0
+      treatedLine = textList[num]
+      treatedLine[1] = str(counter);
+      counter += 1;
+      treatedText.append(' '.join(treatedLine))
+    ficheiro.close()
+    treatedFile = open("treated"+archiveName,"w")
+
+    for member in treatedText:
+      treatedFile.write(member)
+
+
+    treatedFile.close()
