081689efbb1b1094c5d78710dbb28e2b7394a67b
JVSREco19 <joao.victor.silveira.r@unifei.edu.br>
2022-10-14 15:11:08 -0300

    Deleting treated files and running the code for one file each time

--- teste.py
+++ teste.py
@@ -1,5 +1,4 @@
 import re
-import os
 
 def remove_repetidos(lista):
     l = []
@@ -17,69 +16,69 @@ def remove_repetidos(lista):
     print(str(removeCounter)+ " Removed")
     return l
 
-for filename in os.listdir(os.getcwd()):
-  aux = ['template','ligations','counter']
-  archiveName =  filename
-  if archiveName[len(archiveName)-3:len(archiveName)] =='txt' and archiveName[0:7]!='treated':
-    print("Opening {} file".format(archiveName))
-    ficheiro = open(archiveName)
-    text = ficheiro.readlines()
-    linesToMantain = []
-    repeatedLines = []
-    if(archiveName[-3:]=='csv'):
-      text = text[2:]
 
-    activeSites = []
-    counter = 0;
-    textList =[]
-    for linha in text:
-      words = linha.split()
-      textList.append(linha.split())
-      stringWithSpaces= " ".join(words[3:])
-      templateMatch = re.search(r'\b[a-zA-Z][a-zA-Z0-9]{3}\b|\b[a-zA-Z0-9][a-zA-Z][a-zA-Z0-9]{2}\b|\b[a-zA-Z0-9]{2}[a-zA-Z][a-zA-Z0-9]\b|\b[a-zA-Z0-9]{3}[a-zA-Z]\b',stringWithSpaces)
+aux = ['template','ligations','counter']
+archiveName =  'ActiveSitesFound.txt'
+if archiveName[len(archiveName)-3:len(archiveName)] =='txt' and archiveName[0:7]!='treated':
+  print("Opening {} file".format(archiveName))
+  ficheiro = open(archiveName)
+  text = ficheiro.readlines()
+  linesToMantain = []
+  repeatedLines = []
+  if(archiveName[-3:]=='csv'):
+    text = text[2:]
 
-      if templateMatch != None:
-        templatePos = templateMatch.start()
-        templateCode = templateMatch.group()
-        ligationsString = stringWithSpaces[:templatePos-1]
-        finalArray= ligationsString.split(';')
-        findHifen = re.search(r'-',''.join(finalArray))
-        if findHifen!= None or templateCode =="NULL":
-          activeSites.insert(counter,finalArray)
-          activeSites[counter]= aux
-        else:
-          templateCode =stringWithSpaces[templatePos:len(stringWithSpaces)-1]
-          finalArray.append(templateCode)
-          activeSites.insert(counter,finalArray)
-          activeSites[counter].sort()
-          activeSites[counter].append(str(counter))
-      else:
+  activeSites = []
+  counter = 0;
+  textList =[]
+  for linha in text:
+    words = linha.split()
+    textList.append(linha.split())
+    stringWithSpaces= " ".join(words[3:])
+    templateMatch = re.search(r'\b[a-zA-Z][a-zA-Z0-9]{3}\b|\b[a-zA-Z0-9][a-zA-Z][a-zA-Z0-9]{2}\b|\b[a-zA-Z0-9]{2}[a-zA-Z][a-zA-Z0-9]\b|\b[a-zA-Z0-9]{3}[a-zA-Z]\b',stringWithSpaces)
+
+    if templateMatch != None:
+      templatePos = templateMatch.start()
+      templateCode = templateMatch.group()
+      ligationsString = stringWithSpaces[:templatePos-1]
+      finalArray= ligationsString.split(';')
+      findHifen = re.search(r'-',''.join(finalArray))
+      if findHifen!= None or templateCode =="NULL":
         activeSites.insert(counter,finalArray)
         activeSites[counter]= aux
-      
-      counter= counter +1;
-    counter = 0;
-
-    remove_repetidos(activeSites)
+      else:
+        templateCode =stringWithSpaces[templatePos:len(stringWithSpaces)-1]
+        finalArray.append(templateCode)
+        activeSites.insert(counter,finalArray)
+        activeSites[counter].sort()
+        activeSites[counter].append(str(counter))
+    else:
+      activeSites.insert(counter,finalArray)
+      activeSites[counter]= aux
     
-    treatedText = []
+    counter= counter +1;
+  counter = 0;
 
-    for num in linesToMantain:
-      if(counter==100):
-        counter =0
-      treatedLine = textList[num]
-      treatedLine[1] = str(counter);
-      counter += 1;
-      treatedLineStr = ' '.join(treatedLine)
-      treatedText.append(treatedLineStr)
-      
-      
-    ficheiro.close()
-    treatedFile = open("treated"+archiveName,"w")
+  remove_repetidos(activeSites)
+  
+  treatedText = []
 
-
-    for member in treatedText:
-      treatedFile.write(member+'\n')
+  for num in linesToMantain:
+    if(counter==100):
+      counter =0
+    treatedLine = textList[num]
+    treatedLine[1] = str(counter);
+    counter += 1;
+    treatedLineStr = ' '.join(treatedLine)
+    treatedText.append(treatedLineStr)
     
-    treatedFile.close()
     
+  ficheiro.close()
+  treatedFile = open("treated"+archiveName,"w")
+
+
+  for member in treatedText:
+    treatedFile.write(member+'\n')
+  
+  treatedFile.close()
+  
